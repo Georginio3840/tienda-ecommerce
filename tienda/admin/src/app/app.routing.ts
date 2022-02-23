d435c7ce7f77beebbe6cbc5f3d2d8cfde9e405d8
Georginio3840 <georginiotroya@gmail.com>
2022-02-22 22:32:02 -0500

    ADD GUARD AND SERVICES

--- tienda/admin/src/app/app.routing.ts
+++ tienda/admin/src/app/app.routing.ts
@@ -1,7 +1,7 @@
 import {Routes,RouterModule } from "@angular/router";
 import {ModuleWithProviders  } from "@angular/core";
 import { InicioComponent } from "./components/inicio/inicio.component";
-import { LoginComponent } from "./components/login/login.component";
+import {LoginComponent  } from "./components/login/login.component";
 
 const appRoute : Routes=[
     {path:'',component:InicioComponent },
--- tienda/admin/src/app/components/login/login.component.ts
+++ tienda/admin/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
+import { Token } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminService } from 'src/app/services/admin.service';
 declare  var jQuerry:any;
 declare var $:any;
 declare var iziToast:any;
@@ -12,18 +15,60 @@ declare var iziToast:any;
 export class LoginComponent implements OnInit {
 
   public user:any={};
-  constructor() { 
-
+  public usuario:any={};
+  public token:any='';
+  
+  constructor(
+    private _adminService:AdminService,
+    private _router: Router
+  ) { 
+   this.token = this._adminService.getToken();
 
   }
 
   ngOnInit(): void {
+
+    if(this.token){
+      this._router.navigate(['/'])
+    }else{
+      //MANTENER EN COMPONENTE
+      
+    }
+
   }
 
   login(loginForm:any){
  if(loginForm.valid){
    console.log(this.user);
-  alert('Si es valido')
+   let data={
+     email:this.user.email,
+     password: this.user.password
+   }
+  this._adminService.login_admin(data).subscribe(
+    response=>{
+      if(response.data==undefined){
+        iziToast.show({
+          title:'ERROR',
+          titleColor:'#FF0000',
+          color:'#FF',
+          clase:'text-danger',
+          position:'topRight',
+          message:response.message
+        })
+      }else{
+        this.usuario= response.data;
+        localStorage.setItem('token',response.token);
+        localStorage.setItem('_id',response.data._id);
+        this._router.navigate(['/']);
+        
+
+      }
+      console.log(response);
+    },
+    error=>{
+      console.log(error);
+    }
+  );
 
  }else{
    iziToast.show({
